404d9960cf52fc8145c63c68f9eaa63e2ce3751a
mishraatharva <mishraatharva825@gmail.com>
2023-07-29 10:24:48 +0530

    cal route

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome To The Flask"
+
+@app.route('/cal',methods=['GET'])
+def mathoperation():
+    operation = request.json["opearation"]
+    number1 = request.json["number1"]
+    number2 = request.json["number2"]
+
+    if operaion == 'add' :
+        result = number1 + number2
+    
+    elif operation == 'multiply':
+        result = number1 * number2
+    
+    elif operation == "devision":
+        result = number1/number2
+    
+    else:
+        result = number1 - number2
+    
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
